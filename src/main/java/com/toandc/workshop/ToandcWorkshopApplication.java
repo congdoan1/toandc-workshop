61f2f05d1003b12168204a6d1214fed81b6cc293
ToanDC <toandcse61278@gmail.com>
2017-03-31 18:17:36 +0700

    Create project

---
+++ src/main/java/com/toandc/workshop/ToandcWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.toandc.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToandcWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToandcWorkshopApplication.class, args);
+	}
+}
